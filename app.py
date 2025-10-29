7d61e69daa20039bb71a8d72cb56d733191fc068
gaeun020222-sketch <gaeun020222@gmail.com>
2025-10-29 14:26:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.write("Streamlit supports a wide range of data visualizations, including Plot
 all_users = ["Alice", "Bob", "Charly"]
 with st.container(border=True):
     users = st.multiselect("Users", all_users, default=all_users)
-    rolling_average = st.togglle("Rolling average")
+    rolling_average = st.toggle("Rolling average")
 
 np.random.seed(42)
 data = pd.DataFrame(np.random.randn(20, len(users)), columns=users)
@@ -27,4 +27,5 @@ if rolling_average:
 tab1, tab2 = st.tabs(["Chart", "Dataframe"])
 tab1.line_chart(data, height=250)
 tab2.dataframe(data, height=250, use_container_width=True)
+
 
