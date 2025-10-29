d63eb3d15dbfa1efc6460d90a2e8379ad31b707a
gaeun020222-sketch <gaeun020222@gmail.com>
2025-10-29 14:14:39 +0900

    Enhance Streamlit app with user input and charts
    Added user selection and rolling average functionality with data visualization.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#10/22
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -7,3 +8,24 @@ st.write(pd.DataFrame({
     'first column':[1, 2, 3,4],
     'second column':[10, 20, 30, 40]
 }))
+
+#10/29
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.write("Streamlit supports a wide range of data visualizations, including Plotly, Altair, and Bokeh charts.")
+
+all_users = ["Alice", "Bob", "Charly"]
+with st.container(border=True):
+    users = st.multiselect("Users", all_users, default=all_users)
+    rolling_average = st.togglle("Rolling average")
+
+np.random.seed(42)
+data = pd.DataFrame(np.random.randn(20, len(users)), columns=users)
+if rolling_average:
+    data = data.rolling(7).mean().dropna()
+
+tab1, tab2 = st.tabs(["Chart", "Dataframe"])
+tab1.line_chart(data, height=250)
+tab2.dataframe(data, height=250, use_container_width=True)
