225580dbfb8ae75ad914a365287c604a1fe1e585
gaeun020222-sketch <gaeun020222@gmail.com>
2025-10-29 16:11:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("Got lots of data? Great! Streamlit can show [dataframs] with hundred thousands of rows, images, sparklines-and even supports editing!")
+st.write("Got lots of data? Great! Streamlit can show [dataframes] with hundred thousands of rows, images, sparklines-and even supports editing!")
 
 num_rows = st.slider("Number of rows", 1, 10000, 500)
 np. random.seed(42)
@@ -45,14 +45,14 @@ for i in range(num_rows):
             "Preview": f"http://picsum.photos/400/200?lock={i}",
             "Views": np.random.randint(0, 1000),
             "Active": np.random.choice([True, False]),
-            "Category": np.ramdom.choice(["LLM", "Data", "Tool"])
-            "Progress": np.random.randiant(1, 100),
+            "Category": np.random.choice(["LLM", "Data", "Tool"])
+            "Progress": np.random.randint(1, 100),
         }
     )
 data = pd.DataFrame(data)
 
 config = {
-    "Preview": st.column_config.ImageColimn(),
+    "Preview": st.column_config.ImageColumn(),
     "Progress": st.column_config.ProgressColumn(),
 }
 
@@ -60,3 +60,4 @@ if st.toggle("Enable editing"):
     edited_data = st.data_editor(data, column_config=config, use_container_width=True)
 else:
     st.dataframe(data, column_config=config, use_container_width=True)
+
